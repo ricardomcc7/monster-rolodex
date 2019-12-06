ff21f596eb0681208eae86af7edb896b104100ab
ricardomcc7 <codingdata7@gmail.com>
2019-12-05 20:27:50 -0600

    adding files for github pages

---
+++ build/precache-manifest.a304190dbd8527035278cf398a0b00d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93671d1c37e8c1b25c4bb7c93174b70",
+    "url": "/github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "84bd8bf921c172b523c5",
+    "url": "/github.io/monster-rolodex/static/css/main.8fb14636.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/github.io/monster-rolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "84bd8bf921c172b523c5",
+    "url": "/github.io/monster-rolodex/static/js/main.c925e407.chunk.js"
+  },
+  {
+    "revision": "9cffea57ba4facf91ecb",
+    "url": "/github.io/monster-rolodex/static/js/runtime-main.f53d09c7.js"
+  }
+]);
